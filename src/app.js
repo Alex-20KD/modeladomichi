e79c4dfe7fdec6d768835e7228ce43416e0ab2dd
Alex-20KD <gonzaloyzam.2018@gmail.com>
2025-06-06 09:47:45 -0500

    Actualización en app.js antes del rebase

--- src/app.js
+++ src/app.js
@@ -19,3 +19,4 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
+// para subir commit 
